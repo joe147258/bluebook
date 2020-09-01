653b8822337f4e7fd94705d5aa70907171aa51c1
Joe Phillips <joe147258@hotmail.co.uk>
2020-09-01 12:02:33 +0100

    removed test comment

--- src/main/java/com/bluebook/service/QuestionService.java
+++ src/main/java/com/bluebook/service/QuestionService.java
@@ -92,10 +92,8 @@ public class QuestionService {
 
         if(questionString == null || questionString.length() == 0) return false;
         if(correctAnswer == null || correctAnswer.length() == 0) return false;
-        if(!(correctAnswer.equals("TRUE") || correctAnswer.equals("FALSE"))) {
-            System.out.println("SWAG");
-            return false;
-        }
+        if(!(correctAnswer.equals("TRUE") || correctAnswer.equals("FALSE"))) return false;
+        
         if(correctAnswer.contains(";") || questionString.contains(";")) 
             return false;
         
